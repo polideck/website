f7a4642aec04fea18bc9c6dab6dee8eeb704f091
polideck <64500662+polideck@users.noreply.github.com>
2020-10-21 17:28:53 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+document.getElementsByClassName('heading')[0].addEventListener('click', () => {
+    window.location.href = "./"
+});
+
+document.getElementsByClassName('pageButton')[2].addEventListener('click', () => {
+    window.location.href = "./solutions"
+});
+
+document.getElementsByClassName('pageButton')[1].addEventListener('click', () => {
+    window.location.href = "./documentation"
+});
+
+document.getElementsByClassName('pageButton')[0].addEventListener('click', () => {
+    window.location.href = "./contact"
+});
+
+document.getElementsByClassName('loginButton')[0].addEventListener('click', () => {
+    window.location.href = "http://localhost:8080/auth/realms/polideck/protocol/openid-connect/auth?client_id=account&redirect_uri=http%3A%2F%2Flocalhost%3A8080%2Fauth%2Frealms%2Fpolideck%2Faccount%2Flogin-redirect&state=0%2F7ab96341-cef4-4fda-9fef-eb5b0081ea40&response_type=code&scope=openid";
+});
